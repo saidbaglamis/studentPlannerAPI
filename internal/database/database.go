d73b7b9c4a491b110fa1f0821957ff8c849d7552
Said <said@mail>
2024-05-31 11:15:36 +0300

    add gorm tag

--- internal/database/database.go
+++ internal/database/database.go
@@ -3,18 +3,22 @@ package database
 import (
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
-	models2 "studentPlanner/internal/models"
+	"log"
+	"studentPlanner/internal/models"
 )
 
 var Db *gorm.DB
 var err error
 
 func InitDB() {
-	dsn := "root:password@tcp(127.0.0.1:3306)/SKETCH?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:password@tcp(127.0.0.1:3306)/plannerDB?charset=utf8mb4&parseTime=True&loc=Local"
 	Db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("failed to connect database")
 	}
 
-	Db.AutoMigrate(&models2.Student{}, &models2.Plan{})
+	err := Db.AutoMigrate(&models.Student{}, &models.Plan{})
+	if err != nil {
+		log.Fatalf("Failed to auto migrate: %v", err)
+	}
 }
--- internal/models/Plan.go
+++ internal/models/Plan.go
@@ -1,10 +1,12 @@
 package models
 
+import "time"
+
 type Plan struct {
-	ID         uint   `gorm:"primaryKey"`
-	StudentID  uint   `gorm:"not null"`
-	PlanInfo   string `gorm:"type:text;not null"`
-	PlanStatus string `gorm:"type:text;not null"`
-	StartDate  uint   `gorm:"check:number >= 0 AND number <= 2400"`
-	EndDate    uint   `gorm:"check:number >= 0 AND number <= 2400"`
+	ID         uint      `gorm:"primaryKey"`
+	StudentID  uint      `gorm:"not null"`
+	PlanInfo   string    `gorm:"type:text;not null"`
+	PlanStatus string    `gorm:"type:text;not null"`
+	StartDate  time.Time `gorm:"column:start_date;not null"`
+	EndDate    time.Time `gorm:"coloumn:end_date;not null"`
 }
