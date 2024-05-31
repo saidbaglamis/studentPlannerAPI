1f3c62f96be04c8d7d3f3a87e717a4338bf3ae37
Said <said@mail>
2024-05-31 14:31:22 +0300

    Add Name property

--- internal/models/Student.go
+++ internal/models/Student.go
@@ -2,6 +2,7 @@ package models
 
 type Student struct {
 	ID           uint   `gorm:"primaryKey"`
+	Name         string `gorm:"size:255;not null"`
 	Email        string `gorm:"size:255;not null"`
 	SchoolNumber string `gorm:"size:50;not null"`
 	Class        string `gorm:"size:50;not null"`
